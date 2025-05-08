922c36a9d01fedb225e27a8fe5bc47ee90664efd
Yatish Chhibber <yatishchhibber20@gmail.com>
2025-05-08 22:01:19 +0530

    Animations

--- src/index.js
+++ src/index.js
@@ -3,18 +3,51 @@ import { Stage } from "./entities/Stage.js";
 import { Dha } from "./entities/fighters/dha.js";
 import { FpsCounter } from "./entities/FpsCounter.js";
 import { STAGE_FLOOR } from "./constants/stage.js";
-import { FighterDirection } from "./constants/fighter.js";
+import { FighterDirection, FighterState } from "./constants/fighter.js";
+
+function populateMoveDropdown() {
+    const dropdown = document.getElementById('state-dropdown');
+
+    Object.entries(FighterState).forEach(([, value]) => {
+        const option = document.createElement('option');
+        option.setAttribute('value', value);
+        option.innerText = value;
+        dropdown.appendChild(option);
+    }
+);
+}
+
+function handleFormSubmit(event, fighters) {
+    event.preventDefault();
+
+    const selectedCheckboxes = Array
+    .from(event.target.querySelectorAll('input:checked'))
+    .map(checkbox => checkbox.value);
+
+    const options = event.target.querySelector('select');
+
+    fighters.forEach(fighter => {
+        if (selectedCheckboxes.includes(fighter.name)) {
+            fighter.changeState(options.value);
+        }
+    });
+}
 
 window.addEventListener('load', function() {
+    populateMoveDropdown();
     const canvasEl = document.querySelector('canvas');
     const context = canvasEl.getContext('2d');
 
     context.imageSmoothingEnabled = false;
 
+    const fighters = [
+        new Ryu(104, STAGE_FLOOR, FighterDirection.LEFT),
+        new Dha(280,STAGE_FLOOR, FighterDirection.RIGHT),
+    ];
+
     const entities = [
       new Stage(),
-      new Ryu(104, STAGE_FLOOR, FighterDirection.LEFT),
-      new Dha(280,STAGE_FLOOR, FighterDirection.RIGHT),
+      ...fighters,
       new FpsCounter(),
     ]
     let frametime = {
@@ -37,6 +70,8 @@ window.addEventListener('load', function() {
 
         console.log(time);
     }
+
+    this.document.addEventListener('submit',(event) => handleFormSubmit(event, fighters));
 
     window.requestAnimationFrame(frame);
 });
